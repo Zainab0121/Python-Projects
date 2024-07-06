2a946c2c86c624bfeff76263ce9d7dc1080efbda
Zainab0121 <sc20001212021@student.mut.ac.ke>
2024-07-06 17:40:24 +0300

    Lesson 2: 2:32:47

---
+++ Lists/lists.py
@@ -0,0 +1,20 @@
+lucky_numbers =[4, 8, 15, 16, 23, 42]
+friends = ["Kelvn", "Karen", "Jim", "Oscar","Oscar" "Toby"]
+friends[1] = "Mike"
+print(friends[1:3])
+friends.extend(lucky_numbers)
+friends.append("Creed")
+friends.insert(1, "Kelly")
+friends.remove("Jim")
+friends.pop()
+friends.clear()
+friends2 = friends.copy()
+print(friends.index("Oscar"))
+print(friends.count("Oscar"))
+lucky_numbers.sort()
+lucky_numbers.reverse()
+
+print(friends)
+print(lucky_numbers)
+
+print ("Code might not work. This is just a summary of list functions")
---
+++ Tuples/tuples.py
@@ -0,0 +1,2 @@
+coordinates = [(4, 5), (6, 7), (40, 30)]
+print(coordinates[0])
