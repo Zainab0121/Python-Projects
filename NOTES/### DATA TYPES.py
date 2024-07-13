19579850cc9abbd1b7258d19fbf5853b0039426b
Zainab0121 <sc20001212021@student.mut.ac.ke>
2024-07-13 14:26:04 +0300

    notes folder added

---
+++ NOTES/### DATA TYPES.py
@@ -0,0 +1,45 @@
+### INTEGERS
+# An integer is a whole number (not a fraction) that can be positive, negative, or
+# zero. Integers are written without a decimal point.
+# 1, 2, 3, 4, 5, 6,
+# -1, -2, -3, -4, -5, -6, -
+# 0
+# Integers are used to count things, like the number of people in a room, the number
+# of books on a shelf, or the number of miles you've run. They are also used
+# to represent quantities, like the number of apples in a basket, the number of
+# dollars in your wallet, or the number of hours you've been awake.
+
+
+### FLOAT
+# A float is a number that has a decimal point. Floats are used to represent
+# quantities that are not whole numbers, like the price of an item, the
+# temperature outside, or the amount of time it takes to run a mile.
+# 1.0, 2.0, 3.0, 4.0
+
+### STRINGS
+# A string is a sequence of characters. Strings are used to represent text,
+# like the name of a person, the title of a book, or the lyrics of a song
+# "Hello", "World", "Python", "3.6", "3.7",
+
+### LISTS
+# A list is a collection of items. Lists are used to store multiple values in a
+# single variable. Lists are created using square brackets ([]).
+# [1, 2, 3, 4, 5, 6,
+# -1, -2, -3, -4, -5, -6, -
+
+### TUPLES
+# A tuple is a collection of items. Tuples are used to store multiple values in a
+# single variable. Tuples are created using parentheses (()). Tuples are
+# immutable, which means that once a tuple is created, its items cannot be changed.
+# (1, 2, 3, 4, 5, 6,
+
+### DICTIONARY
+# A dictionary is a collection of key-value pairs. Dictionaries are used to store
+# data in a structured way. Dictionaries are created using curly braces ({}).
+# {"name": "John", "age": 30, "city": "New York"
+
+
+### BOOLEAN
+# A boolean is a data type that can have only two values: True or False. Booleans
+# are used to represent logical values, like whether a condition is true or false.
+# True, False
